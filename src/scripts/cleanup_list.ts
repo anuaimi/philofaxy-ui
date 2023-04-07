737d8d0e7f5a6d19b5829df1faa6cf3080170051
Athir Nuaimi <athir@nuaimi.com>
2023-04-07 11:40:52 -0400

    added a lot of the files from philofaxy for 2023.  still need a few more but their server is blocking right now

--- src/scripts/cleanup_list.ts
+++ src/scripts/cleanup_list.ts
@@ -1,4 +1,9 @@
 const url = require('url');
+const fs = require('fs');
+
+const {execSync} = require('child_process');
+const { Readable } = require('stream');
+const { finished } = require('stream/promises');
 
 // enum PaperSize { A4, A5, Personal, Pocket };
 
@@ -18,9 +23,31 @@ const data = require("../../data/philofaxy.json");
 // for each url, remove the host and the path
 // console.log(data);
 
-function downloadFile( sourceUrl: string, targetFilename: string): boolean {
+ // downloadFile - download the requested file & save to disk
+async function downloadFile( sourceUrl: string, targetFilename: string): Promise<boolean> {
 
-  // download the file  - save to disk
+  console.log("downloading: ", sourceUrl);
+
+  // see if file already exists
+  if (fs.existsSync(targetFilename)) {
+    // console.log('  have file already');
+    return true;
+  }
+
+  // need to download the file
+  const response = await fetch(sourceUrl);
+  if (response.ok) {
+    const stream = fs.createWriteStream(targetFilename);
+    await finished(Readable.fromWeb(response.body).pipe(stream));  
+  } else {
+    console.log("  error downloading file: ",response.statusText);
+    return false;
+  }
+
+  // console.log("  downloaded file");
+
+  // handle grumpy servers
+  await execSync('sleep 4');
 
   return true;
 }
@@ -41,7 +68,24 @@ function generateLocalFilename( name: string, paperSize: string, sourceUrl: stri
     return localFilename;
 }
 
-function main() {
+async function main() {
+
+  // const sourceUrl = "https://philofaxy.com/inserts/__2020%20revamp/1.1%20day%20per%20page/Picture%201.png";
+  // const response = await fetch(sourceUrl);
+  // if (response.ok) {
+  //   if (response.body === undefined) {
+  //     console.log('no stream');
+  //   } else {
+  //     console.log('ready to get body');
+  //     console.log(response.body);
+  //     const stream = fs.createWriteStream("./test.png");
+  //     await finished(Readable.fromWeb(response.body).pipe(stream));
+  //     console.log("file written");
+  //   }
+  // } else {
+  //   console.log(response.statusText);
+  // }
+  // console.log("==========");
 
   // for each planner template
   for (var entry in data) {
@@ -51,24 +95,27 @@ function main() {
     console.log('getting files for: ', name, " (", paperSize, ")");
 
     let sourceUrl = data[entry]['preview'];
-    let localFilename = generateLocalFilename( name, paperSize, sourceUrl);
-    downloadFile( sourceUrl, localFilename);
+    let localFilename = await generateLocalFilename( name, paperSize, sourceUrl);
+    await downloadFile( sourceUrl, localFilename);
 
     sourceUrl = data[entry]['inserts-pdf'];
-    localFilename = generateLocalFilename( name, paperSize, sourceUrl);
-    downloadFile( sourceUrl, localFilename);
+    localFilename = await generateLocalFilename( name, paperSize, sourceUrl);
+    await downloadFile( sourceUrl, localFilename);
 
     sourceUrl = data[entry]['inserts-word'];
-    localFilename = generateLocalFilename( name, paperSize, sourceUrl);
-    downloadFile( sourceUrl, localFilename);
+    localFilename = await generateLocalFilename( name, paperSize, sourceUrl);
+    await downloadFile( sourceUrl, localFilename);
 
     sourceUrl = data[entry]['template-word'];
-    localFilename = generateLocalFilename( name, paperSize, sourceUrl, "_template");
-    downloadFile( sourceUrl, localFilename);
+    localFilename = await generateLocalFilename( name, paperSize, sourceUrl, "_template");
+    await downloadFile( sourceUrl, localFilename);
 
     sourceUrl = data[entry]['template-excel'];
-    localFilename = generateLocalFilename( name, paperSize, sourceUrl, "_template");
-    downloadFile( sourceUrl, localFilename);
+    localFilename = await generateLocalFilename( name, paperSize, sourceUrl, "_template");
+    await downloadFile( sourceUrl, localFilename);
+
+    // don't want to get blocked by the server
+    // await execSync('sleep 5');
   }
 
 }
